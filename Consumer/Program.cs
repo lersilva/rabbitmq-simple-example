67883a65e275600df36ac61db10a1e5514ea8cb9
Leozin <leoabyss@gmai.com>
2023-06-29 14:32:03 -0300

    First commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var ConnectionFactory = factory.CreateConnection();
+
+using var channel = ConnectionFactory.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox", durable:false, exclusive:false, autoDelete:false, arguments:null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Messages Received: {message}");
+};
+
+channel.BasicConsume(queue: "letterbox", autoAck: true, consumer: consumer);
---
+++ Producer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var ConnectionFactory = factory.CreateConnection();
+
+using var channel = ConnectionFactory.CreateModel();
+
+channel.QueueDeclare(queue: "letterbox", durable:false, exclusive:false, autoDelete:false, arguments:null);
+
+var message = "Hello World Message";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);  
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
+
